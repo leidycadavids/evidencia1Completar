05f21ced0cce55bdfabceac5d72beb859c332cb1
LEIDY CADAVID <leidy.cadavids@est.iudigital.edu.co>
2025-05-10 19:21:02 -0500

    Se modifico la linea 5 agregando la palabra inventario se modifico la linea 10 agregando la palabra inventario en la linea 62 se puso  la palabra marca                                                                En 16 se agrega null para iniciar la variable     en la linea 19 se agrego a codigo vehiculo          en la linea 27 se agrego la palabra inventario

--- src/Concesionario.java
+++ src/Concesionario.java
@@ -2,8 +2,8 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Concesionario {
-    private List<Vehiculo> inventario= new ArrayList<>(); // Lista de vehículos disponibles
-    private List<Venta> ventas = new ArrayList<>(); // Lista de ventas realizadas
+    private final  List<Vehiculo> inventario= new ArrayList<>(); // Lista de vehículos disponibles
+    private  final List<Venta> ventas = new ArrayList<>(); // Lista de ventas realizadas
 
     // Método para agregar un vehículo al inventario
     public void agregarVehiculo(Vehiculo vehiculo) {
